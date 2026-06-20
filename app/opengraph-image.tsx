f74a3e83c6e465470ba0f2b1853c1aa690f78141
TransferNova <transfernovahq@gmail.com>
2026-06-20 11:45:47 +0200

    feat: revalidate og:image every hour

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -6,6 +6,7 @@ export const runtime = 'nodejs'
 export const alt = 'TransferNova - Rumores en tiempo real'
 export const size = { width: 1200, height: 630 }
 export const contentType = 'image/png'
+export const revalidate = 3600
 
 async function getStats() {
   try {
